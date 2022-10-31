11f4e29c764b6f9d2c426ca04f34cc41c374fbfe
Pedro Ivo <41927669+pedroisbraga@users.noreply.github.com>
2022-10-31 17:04:40 -0300

    About-Me
    Aplicação para entrada de parâmetros de características pessoais.

---
+++ about-me/src/SobreMim.java
@@ -0,0 +1,18 @@
+public class SobreMim {
+    public static void main(String[] args) {
+
+      // String nome = args [0];
+      // String sobrenome = args [1];
+      // int idade = Integer.valueOf(args[2]);
+     //  double altura = Double.valueOf(args[3]);
+
+        String nome = "Pedro" ;
+        String sobrenome = "Braga" ;
+        int idade = 30 ;
+        double altura = 1.69 ;
+
+        System.out.println("Olá, me chamo" + nome + sobrenome);
+        System.out.println("Tenho" + idade +"anos");
+        System.out.println("Minha altura é" + altura + "cm");
+    }
+}
